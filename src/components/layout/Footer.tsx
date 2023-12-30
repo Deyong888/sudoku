3652f3a03d3eb525d83586fffa52b059351f8e70
Deyong888 <xzywork@163.com>
2023-12-30 20:05:30 +0800

    添加备案号

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 export const Footer = () => {
   return (
     <footer className="footer">
-      <p>&#169; 2023 麒麟剑 </p>
+      <p>&#169; 2023 麒麟剑 <a href="https://beian.miit.gov.cn/" rel="nofollow">粤ICP备2023090172号 粤公网安备 44011402000890号</a></p>
     </footer>
   )
 }
