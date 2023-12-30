3d5623aba8be20efb8692471c7826e832c887e4a
Deyong888 <xzywork@163.com>
2023-12-30 23:26:54 +0800

    修改英文介绍

--- src/IntroduceSudoku.tsx
+++ src/IntroduceSudoku.tsx
@@ -3,19 +3,36 @@ import React from 'react';
 export const Introduce = () => {
     return (
         <div>
-            <h2>数独游戏介绍</h2>
-            <p>
-                <br />
-                数独是一款经典的逻辑游戏，起源于 19 世纪的瑞士。游戏的目标是将数字从 1 到 9
-                填入一个 9x9 的网格中，使得每一行、每一列和每个 3x3 的子网格都包含数字 1 到
-                9，且不重复。
-                <br />
-                <br />
-                数独游戏通常会给定一些数字作为提示，玩家需要根据这些提示来推导出其他数字。数独游戏有许多不同的难度等级，从简单到困难不等，适合不同水平的玩家。
-                <br />
-                <br />
-                数独游戏不仅可以锻炼逻辑思维能力，还可以帮助人们放松身心。如果您正在寻找一款益智游戏来打发时间，那么数独是一个不错的选择。
-            </p>
+            <div>
+            <br/>
+            <h2>What is Sudoku</h2>
+            <p>Sudoku is a logic-based, combinatorial number-placement puzzle. In classic Sudoku, the objective is to fill a 9×9 grid with digits so that each column, each row, and each of the nine 3×3 subgrids that compose the grid (also called "boxes", "blocks", or "regions") contain all of the digits from 1 to 9. The puzzle setter provides a partially completed grid, which for a well-posed puzzle has a single solution.</p>
+            </div>
+            <div>
+            <br/> <h2>How to play sudoku</h2> 
+            <ol>
+                <li><strong>Scan the puzzle for easy numbers. </strong>Look for rows, columns, or boxes that have only one possible value for a cell. These are called "singles." Fill in these cells with the appropriate numbers.</li>
+                <li><strong>Use pencil marks to note possible values for each cell. </strong>As you solve the puzzle, you may come across cells that have more than one possible value. Mark these cells with pencil marks to remind yourself of the possibilities.</li>
+                <li><strong>Use logic to eliminate impossible values.</strong> Look for cells where a certain value cannot be placed because it would violate the rules of Sudoku. For example, if a row already contains the digits 1, 2, and 3, then no other cell in that row can contain those digits.</li>
+                <li><strong>Use advanced techniques to solve the puzzle. </strong>As you become more experienced in solving Sudoku puzzles, you can learn advanced techniques that can help you solve even the most difficult puzzles. Some of these techniques include:</li>
+                <ul>
+                    <li><strong>X-Wing: </strong>This technique is used to eliminate impossible values in cells that are in the same row or column as two cells that contain the same value.</li>
+                    <li><strong>Swordfish: </strong>This technique is used to eliminate impossible values in cells that are in the same box as three cells that contain the same value.</li>
+                    <li><strong>Hidden Singles:</strong> This technique is used to find values that are the only possible value for a cell, even though they are not the only possible value for any other cell in the same row, column, or box.</li>
+                </ul>
+                <li><strong>Be patient and persistent. </strong>Sudoku puzzles can be challenging, but they are also very rewarding to solve. If you get stuck, take a break and come back to the puzzle later. With practice, you will become faster and more skilled at solving Sudoku puzzles.</li>
+            </ol>
+            </div>
+            <div>
+            <br/><h2>Additional tips for solving Sudoku puzzles:</h2> 
+            <ul>
+                <li><strong>Start with the easiest puzzles.</strong> As you become more experienced, you can try more difficult puzzles.</li>
+                <li><strong>Take breaks. </strong>If you get stuck, take a break and come back to the puzzle later.</li>
+                <li><strong>Use a pencil.</strong> Pencil marks will help you keep track of possible values for each cell.</li>
+                <li><strong>Don't be afraid to make mistakes. </strong>Everyone makes mistakes when solving Sudoku puzzles. Just learn from your mistakes and move on.</li>
+                <li><strong>Have fun!</strong> Sudoku puzzles are a great way to challenge your mind and have some fun at the same time.</li>
+            </ul>
+            </div>
         </div>
     )
 }
