049878094cb80245cb52e5453ef040f5ad8cbffc
Deyong888 <xzywork@163.com>
2023-12-31 19:29:47 +0800

    修改脚部

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 /**
  * React component for the Footer Section.
+ * <a href="https://beian.miit.gov.cn/" rel="nofollow">粤ICP备2023090172号 粤公网安备 44011402000890号</a>
  */
 export const Footer = () => {
   return (
     <footer className="footer">
-      <p>&#169; 2023 麒麟剑 <a href="https://beian.miit.gov.cn/" rel="nofollow">粤ICP备2023090172号 粤公网安备 44011402000890号</a></p>
+      <p>&#169; 2023年12月31日  麒麟剑 </p>
     </footer>
   )
 }
